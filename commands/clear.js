79f4db79620f6271d2fd8e9c876204d4294560af
ThoniMiguel <wanok8000@outlook.com>
2021-03-08 20:08:49 -0300

    blablabla

--- commands/clear.js
+++ commands/clear.js
@@ -2,6 +2,7 @@ module.exports = {
     name:"clear",
     description: "Clear n messages",
     async execute(msg,args){
+        console.log(msg.author);
         if(!args[0] || isNaN(args[0]) || args[0] > 50 || args[0] < 1){
             return msg.reply("Põem uma porra decente ai, caralho!");
         }
