e5b8bd3d916ad9430aa6d15c5cea03be6cef33d3
ThoniDesktop <wanok8000@outlook.com>
2021-03-10 22:20:12 -0300

    spam.js

--- commands/spam.js
+++ commands/spam.js
@@ -2,6 +2,17 @@ module.exports = {
   name: "spam",
   description: "spam msgs",
   execute(msg, args) {
-    console.log(`${args[0]} ${args[1]} ${args[2]}`);
+    let message = args[0];
+    let number = args[1];
+
+    number = parseInt(number);
+    if (message.length > 20 || isNaN(number) || number > 8) {
+      msg.reply("Comando ta meio errado ai neh... tenta denovo... PIU!");
+      return;
+    } else {
+      for (let i = 0; i < number; i++) {
+        $msg.channel.send(message);
+      }
+    }
   },
 };
