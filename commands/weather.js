f0323f7e86d9abdaa49ec62b81ecad4bbc6571e9
ThoniDesktop <wanok8000@outlook.com>
2021-03-11 04:29:47 -0300

    weatherAPI

---
+++ commands/weather.js
@@ -0,0 +1,26 @@
+const { APIMessage } = require("discord.js");
+const fetch = require("node-fetch");
+
+module.exports = {
+  name: "weather",
+  description: "Weather forecast",
+  execute(msg, args) {
+    api(msg, args);
+  },
+};
+
+function api(msg, args) {
+  const API_KEY = "afdf4bc6fc4046f3c58451311b52990b";
+  if (args.length == 0) {
+    msg.reply("Comando vazio???Quer me fuder porra?!... PIU CARALHO!!");
+    return;
+  } else {
+    let cityName = args[0];
+    let url = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+    fetch(url)
+      .then((data) => data.json())
+      .then((res) => {
+        console.log(res);
+      });
+  }
+}
