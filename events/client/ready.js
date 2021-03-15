12febabd1fda521644bebacda1205014ead36b90
ThoniMiguel <wanok8000@outlook.com>
2021-03-14 21:37:43 -0300

    add sunglasses emoticon

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,4 @@
 module.exports = (Discord, client, args) => {
     console.log("bot is online")
-    client.channels.cache.get("256168112561455104").send("O pai ta on");
+    client.channels.cache.get("256168112561455104").send("O pai ta on :sunglasses:");
 }
