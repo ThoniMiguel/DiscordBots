be95f70b67e1e43f7f80213610ac2150475f3480
ThoniMiguel <wanok8000@outlook.com>
2021-03-14 21:31:35 -0300

    some adjustments

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,4 @@
-module.exports = () => {
+module.exports = (Discord, client, args) => {
     console.log("bot is online")
+    client.channels.cache.get("256168112561455104").send("O pai ta on");
 }
