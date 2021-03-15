968f2a9c7ea8712b2d2ea2014746bc4957c31bda
ThoniMiguel <wanok8000@outlook.com>
2021-03-15 02:35:36 -0300

    env

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const prefix = "$";
 const fs = require("fs");
+require("dotenv").config();
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
