2d629a951a325a6b3909128493c8e709ecf34571
ThoniMiguel <wanok8000@outlook.com>
2021-03-14 22:06:26 -0300

    start mpcheck

--- commands/mpcheck.js
+++ commands/mpcheck.js
@@ -1,5 +1,21 @@
+const cod = require("call-of-duty-api")();
 module.exports = {
   name: "mpcheck",
   description: "This command return some call of duty shit",
-  execute(client, msg, args) {},
+  async execute(client, msg, args, Discord) {
+    if(!args[0]) return msg.channel.send("Poem um username valido ai animal... PIU! :triumph:");
+    if(!args[1]) return msg.channel.send("Poem uma plataforma descente ai seu burro!... PIU! :triumph:")
+
+    let username = process.env.COD_USERNAME;
+    let password = process.env.COD_PASSWORD;
+
+    try{
+      await cod.login(username, password);
+      let data = await cod.CWmp("Wanok#11339", "battle");
+      console.log(data);
+    }catch(err){
+      msg.channel.send("Aconteceu um erro ao buscar esse jogador... :neutral_face: ");
+      throw err;
+    }
+  },
 };
